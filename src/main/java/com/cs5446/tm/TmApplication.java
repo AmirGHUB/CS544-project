d25f7df650a8578376cabedb1529f24ff11ada14
Ammar Atef <ammar.atef45@gmail.com>
2020-05-11 16:49:38 -0500

    Added docker

--- src/main/java/com/cs5446/tm/TmApplication.java
+++ src/main/java/com/cs5446/tm/TmApplication.java
@@ -2,6 +2,9 @@ package com.cs5446.tm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class TmApplication {
@@ -10,5 +13,13 @@ public class TmApplication {
 		SpringApplication.run(TmApplication.class, args);
 		// Test
 	}
+
+	@RestController
+	public class MyController {
+        @GetMapping("/")
+        public String test() {
+            return "working";
+        }
+    }
 
 }
