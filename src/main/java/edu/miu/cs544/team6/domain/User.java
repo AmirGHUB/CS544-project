c04d79a54686787f1f0f23cb8ec4a0dcfd870f0a
KevinJustice25 <kevinjustice25@gmail.com>
2020-05-13 13:01:14 -0500

    commented out most of config for MySQL DB, just left security for hardcoded users

--- src/main/java/edu/miu/cs544/team6/domain/User.java
+++ src/main/java/edu/miu/cs544/team6/domain/User.java
@@ -4,13 +4,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 @Entity
+@Table(name="USERS")
 public class User {
 
 	@Id
