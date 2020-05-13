b6553e2e72f621523226ffe14c7fca7625fbd969
Ochirsuren <ochiroo13@gmail.com>
2020-05-12 21:35:56 -0500

    Email changes

--- src/main/java/edu/miu/cs544/team6/service/Notification.java
+++ src/main/java/edu/miu/cs544/team6/service/Notification.java
@@ -1,6 +1,7 @@
 package edu.miu.cs544.team6.service;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import edu.miu.cs544.team6.domain.EmailNotification;
+import edu.miu.cs544.team6.domain.Reservation;
+import edu.miu.cs544.team6.enums.ENotificationStatus;
 import edu.miu.cs544.team6.repository.NotificationRepository;
 
 @Service
@@ -24,15 +27,27 @@ public class Notification {
 	private NotificationRepository repository;
 
 	@Scheduled(fixedDelayString = "${sync-job.delay.milliseconds.fixed}", initialDelayString = "${sync-job.delay.milliseconds.initial}")
-	public void checkNotification() {
+	public void schSendEmail() {
 
-		List<EmailNotification> lstEmail = repository.findByStatus("PENDING");
+		List<EmailNotification> lstEmail = repository.findByStatus(ENotificationStatus.NEW);
 
 		for (EmailNotification email : lstEmail) {
 			System.out.println(email.toString());
+			sendMail(null);
 		}
 
-		sendMail(null);
+		System.out.println("working schedule: " + LocalDateTime.now());
+	}
+
+	@Scheduled(fixedDelayString = "${sync-job.delay.milliseconds.fixed}", initialDelayString = "${sync-job.delay.milliseconds.initial}")
+	public void schReminder() {
+
+		List<EmailNotification> lstEmail = repository.findByStatus(ENotificationStatus.NEW);
+
+		for (EmailNotification email : lstEmail) {
+			System.out.println(email.toString());
+			sendMail(null);
+		}
 
 		System.out.println("working schedule: " + LocalDateTime.now());
 	}
@@ -43,13 +58,40 @@ public class Notification {
 		SimpleMailMessage message = new SimpleMailMessage();
 		try {
 
-			message.setFrom("fromMail");
-			message.setTo("ochiroo13@gmail.com");
-			message.setSubject("Subje");
-			message.setText("this is content");
+			message.setFrom("TM Appointment System");
+			message.setTo(email.getRecipientEmail());
+			message.setSubject(email.getSubject());
+			message.setText(email.getContent());
 			mailSender.send(message);
+
+			email.setStatus(ENotificationStatus.SUCCEED);
+			updateNotification(email);
+
 		} catch (MailException me) {
 			System.out.println("email exception: " + me);
+
+			email.setStatus(ENotificationStatus.FAILED);
+			updateNotification(email);
 		}
+	}
+
+	@Async
+	public void insertNotification(Reservation reservation) {
+		EmailNotification emailNotif = new EmailNotification();
+		emailNotif.setReservation(reservation);
+		emailNotif.setAppointmentDatetime(reservation.getAppointment().getAppointmentDate());
+		emailNotif.setContent(String.format("Dear %s, Your reservation number %s is %s",
+				reservation.getConsumer().getFirstName(), reservation.getId(), reservation.getStatus()));
+		emailNotif.setCreatedDatetime(new Date());
+		emailNotif.setRecipientEmail(reservation.getConsumer().getEmail());
+		emailNotif.setSentDatetime(null);
+		emailNotif.setStatus(ENotificationStatus.NEW);
+		emailNotif.setSubject(String.format("Decision about reservation id: %s", reservation.getId()));
+		emailNotif.setReservation(reservation);
+		repository.save(emailNotif);
+	}
+
+	public void updateNotification(EmailNotification emailNotif) {
+		repository.save(emailNotif);
 	}
 }
